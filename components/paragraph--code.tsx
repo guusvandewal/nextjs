3ca7f1a93f1b94cca1f3dced62448c2b4725ff20
Guus van de Wal <git@guusvandewal.nl>
2023-02-16 12:50:01 +0100

    Added prismjs

--- components/paragraph--code.tsx
+++ components/paragraph--code.tsx
@@ -1,21 +1,32 @@
 import { ParagraphProps } from "components/paragraph"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Prism from "prismjs"
+
+import "prismjs/components/prism-javascript"
+
+import "prismjs/components/prism-css"
+
+import "prismjs/components/prism-jsx"
+//import "prismjs/plugins/toolbar/prism-toolbar"
+import "prismjs/plugins/line-numbers/prism-line-numbers"
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard"
+
+import "prismjs/themes/prism.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+//import "prismjs/themes/prism-tomorrow.css"
 export function ParagraphCode({ paragraph, ...props }: ParagraphProps) {
-  console.log("paragraph", paragraph)
   const codeType = `language-${paragraph.field_code_type}`
   useEffect(() => {
-    setTimeout(() => Prism.highlightAll(), 50)
-  }, [])
+    const highlight = async () => {
+      await Prism.highlightAll() // <--- prepare Prism
+    }
+    highlight().then(r => console.log("highlighted"))
+  }, [paragraph])
+
   return (
     <section>
       {paragraph.field_code && (
-        <pre className="line-numbers">
-          <code
-            className={codeType}
-            dangerouslySetInnerHTML={{ __html: paragraph.field_code }}
-          />
-        </pre>
+          <pre className="line-numbers"><code className={codeType}>{paragraph.field_code}</code></pre>
       )}
     </section>
   )
